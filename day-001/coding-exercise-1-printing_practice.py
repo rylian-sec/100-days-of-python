3f828aaa2868268e2691a7428dbe7aaffb8859fc
rylian <285080284+rylian-sec@users.noreply.github.com>
2026-06-16 23:06:50 -0400

    Day 1: printing practice, debugging practice, variables, and band name generator

---
+++ day-001/coding-exercise-1-printing_practice.py
@@ -0,0 +1,13 @@
+# This exercise is to practice using the print function. I need to make the ouput read exactly as the recipe below.
+
+#1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+#2. Knead the dough for 10 minutes.
+#3. Add 3g of Salt.
+#4. Leave to rise for 2 hours.
+#5. Bake at 200 degrees C for 30 minutes.
+
+print("""1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+2. Knead the dough for 10 minutes.
+3. Add 3g of Salt.
+4. Leave to rise for 2 hours.
+5. Bake at 200 degrees C for 30 minutes.""")
---
+++ day-001/coding-exercise-3-variables.py
@@ -0,0 +1,10 @@
+#Practice swapping variables. The goal is to make glass2
+# hold milk and glass 1 hold juice, without typing milk or
+# juice.
+
+glass1 = "milk"
+glass2 = "juice"
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
