5917c5dcd656df4d21f1ee2e9e9bb55480ac6076
rylian <285080284+rylian-sec@users.noreply.github.com>
2026-06-19 13:04:02 -0400

    Day 3: Controlling flow with if elif else and logical operators

---
+++ day-003/coding-exercise-5-bmi_calculator_with_interpretations.py
@@ -0,0 +1,25 @@
+# Coding Exercise 5: BMI Calculator with Interpretations
+
+# BMI Calculator with Interpretations
+# Add some if/elif/else statements to the BMI calculator so that it interprets the BMI values calculated.
+# If the bmi is under 18.5 (not including), print out "underweight"
+# If the bmi is between 18.5 (including) and 25 (not including), print out "normal weight"
+# If the bmi is 25 (including) or over, print out "overweight"
+
+
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+
+if bmi < 18.5:
+    print("underweight")
+
+if bmi >= 25:
+    print("overweight")
+
+else:
+    print("normal weight")
---
+++ day-003/day-3-project-treasure_island.py
@@ -0,0 +1,65 @@
+#Your goal today is to build a "Chose your own adventure game". Using what you
+# have learnt in the lessons today you will be building a very simple version of
+# this type of text game.
+
+# Use the flow chart linked here to create the game logic.
+#https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload#%7B%22pageId%22%3A%22C5RBs43oDa-KdzZeNtuy%22%7D
+
+#Once you've completed the project, you can always extend the game and make it
+# more interesting!
+
+#==============================================================================
+
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.\n")
+
+#==============================================================================
+
+# My code, for project completion, begins here.
+
+left_or_right = input("You can go left or right.\n Enter L or R: ")
+
+if left_or_right == "L" or left_or_right == "l":
+    print("\nYou walk for a few hours and....")
+    print("You have come to a dock on a beautiful and vast lake, with an island in the middle.\n")
+    swim_or_wait = input("Would you like to Swim across or Wait for a boat? \n Enter S or W: ")
+
+    if swim_or_wait == "W" or swim_or_wait == "w":
+        print("\nA boat arrives and ferries you to the island.")
+        print("In the center of the island is a small building with three doors, one Red, one Yellow, and one Blue.\n")
+        red_blue_yellow = input("Which would you like to open? \n Enter R, B, or Y: ")
+
+        if red_blue_yellow == "Y" or red_blue_yellow == "y":
+            print("\nYou found the treasure! You Win!")
+        elif red_blue_yellow == "R" or red_blue_yellow == "r":
+            print ("\nA fireball envelopes you.\nGame Over.")
+        else:
+            print("\nA giant tiger jumps out and eats you.\nGame Over.")
+
+    else:
+        print("\nA giant trout swallows you whole.\nGame Over.")
+
+else:
+    print("You fall into a deep hole. \n Game Over.")
